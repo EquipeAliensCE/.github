eb7961152153dcff3d6db5626c029a8411b44737
DaviF132 <gatinhofofinho14121@gmail.com>
2025-09-27 01:38:04 -0300

    Novos arquivos de cena adicionados na pasta

---
+++ Praia de plástico/scenemenu.js
@@ -0,0 +1,31 @@
+const { Physics } = require("phaser");
+
+const size={
+width:500,
+heigth:500,
+}
+
+const speedDown =300
+
+const config = {
+  type: Phaser.WEBGL,
+  width: size.width,
+  heigth: size.width,
+  //Quando for precisar puxar um arquivo do HTML, faça como se fosse JS e HTML normal usando o document.getElementeById.
+  canvas: document.getElementById('gameCanvas'),
+  backgroundColor: '#0d0d35',
+  scene: {
+    create: function () {
+      this.add.text(100, 200, 'Funcionou!', { fontSize: '32px', fill: '#fff' });
+    }
+  },
+  Physics:{
+      default:"arcade",
+      arcade:{
+        gravity:{y:speedDown},
+        debug:true
+      }
+  }
+};
+
+const game = new Phaser.Game(config)
