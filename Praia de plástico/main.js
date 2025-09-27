477702baa5465fee00a7dbeea91a1959ee396502
DaviF132 <gatinhofofinho14121@gmail.com>
2025-09-27 01:32:28 -0300

    Ajeitando a main e adicionandop comandos novos a mesma

--- Praia de plástico/main.js
+++ Praia de plástico/main.js
@@ -1,16 +1,31 @@
+const { Physics } = require("phaser");
+
+const size={
+width:500,
+heigth:500,
+}
+
+const speedDown =300
+
 const config = {
   type: Phaser.WEBGL,
-  width: 500,
-  height: 500,
+  width: size.width,
+  heigth: size.width,
+  //Quando for precisar puxar um arquivo do HTML, faça como se fosse JS e HTML normal usando o document.getElementeById.
   canvas: document.getElementById('gameCanvas'),
   backgroundColor: '#0d0d35',
   scene: {
     create: function () {
       this.add.text(100, 200, 'Funcionou!', { fontSize: '32px', fill: '#fff' });
     }
+  },
+  Physics:{
+      default:"arcade",
+      arcade:{
+        gravity:{y:speedDown},
+        debug:true
+      }
   }
 };
 
-new Phaser.Game(config);
-
-console.log("Phaser versão:", Phaser.AUTO);
+const game = new Phaser.Game(config)
