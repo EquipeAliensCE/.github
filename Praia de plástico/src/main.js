56c81f139f47109c21a89a2a3dc56b5483d806ca
DaviF132 <gatinhofofinho14121@gmail.com>
2025-09-26 23:09:50 -0300

    Criando o style e ajeitando a main

--- Praia de plástico/src/main.js
+++ Praia de plástico/src/main.js
@@ -1,1 +1,16 @@
 import './style.css'
+import Phaser from 'phaser'
+
+const size={
+ width:500,
+ height:500,
+}
+
+const config = {
+  type:Phaser.WEBGL,
+  width:size.width,
+  height:size.height,
+  canvas:gameCanvas
+}
+
+const game = new Phaser.Game(config)
